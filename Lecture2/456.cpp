da46607f72cb750d10408ddd2f9784f6567dd106
Chung Chan <chung.chan@cityu.edu.hk>
2024-09-07 15:57:19 +0800

    Lecture2 and Lab1

---
+++ Lecture2/456.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "4" + "5" + 6 << std::endl;
+}
---
+++ Lecture2/456.java
@@ -0,0 +1,5 @@
+class MyAddition {
+    public static void main(String[] args) {
+        System.out.println("4" * "5" * 6);
+    }
+}
