74f60c7da19e6e86a9a17103b8b0d7b5cf961fa0
Chung Chan <chung.chan@cityu.edu.hk>
2024-09-15 10:00:34 +0800

    fix flowcharts

--- Lecture3/flowcharts.py
+++ Lecture3/flowcharts.py
@@ -3,8 +3,8 @@ from divewidgets import create_flowchart
 
 sort_two_values_fc1 = create_flowchart(code=r"""
 st=>start: Start
-cond1=>condition: if (x <= y)
-cond2=>condition: if (y < x)
+cond1=>condition: x <= y
+cond2=>condition: y < x
 suite1=>inputoutput: print(x, y)
 suite2=>inputoutput: print(y, x)
 e=>end
@@ -18,7 +18,7 @@ cond2(no)->e
 
 sort_two_values_fc2 = create_flowchart(code=r"""
 st=>start: Start
-cond1=>condition: if (x <= y)
+cond1=>condition: x <= y
 suite1=>inputoutput: print(x, y)
 suite2=>inputoutput: print(y, x)
 e=>end
@@ -30,15 +30,15 @@ cond1(no)->suite2(bottom)->e
 
 sort_three_values_fc = create_flowchart(code=r"""
 st=>start: Start
-cond1=>condition: if (x <= y <= z)
+cond1=>condition: x <= y <= z
 suite1=>inputoutput: print(x, y, z)
-cond2=>condition: if (x <= z <= y)
+cond2=>condition: x <= z <= y
 suite2=>inputoutput: print(x, z, y)
-cond3=>condition: if (y <= x <= z)
+cond3=>condition: y <= x <= z
 suite3=>inputoutput: print(y, x, z)
-cond4=>condition: if (y <= z <= x)
+cond4=>condition: y <= z <= x
 suite4=>inputoutput: print(y, z, x)
-cond5=>condition: if (z <= x <= y)
+cond5=>condition: z <= x <= y
 suite5=>inputoutput: print(z, x, y)
 suite6=>inputoutput: print(z, y, x)
 
