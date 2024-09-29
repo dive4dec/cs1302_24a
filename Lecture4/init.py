acf921e9c367eed212b75b186b4d00d4b3bc96ec
Chung Chan <chung.chan@cityu.edu.hk>
2024-09-29 12:15:36 +0800

    update init

--- Lecture4/init.py
+++
@@ -1,7 +0,0 @@
-async def install_dependencies():
-    try:
-        import micropip
-        await micropip.install('divewidgets')
-        await micropip.install('ipywidgets')
-    except ModuleNotFoundError:
-        pass
